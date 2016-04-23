65d0ad3282d450e7e85f72e9c8bb37cf33062c4b
SupaStuff <StuffQilla@gmail.com>
2016-04-23 19:08:52 -0400

    updated vr.js

--- js/vr.js
+++ js/vr.js
@@ -1,1 +1,25 @@
-//hi
+VR.floor();
+VR.box();
+console.log(VR.body.position);
+VR.body.moveX(4);
+VR.body.moveZ(4);
+VR.body.moveY(4);
+VR.box(VR.body.position);
+VR.body.moveZ(8);
+
+VR.box({x:2, color:'red', material: 'stone'});
+
+var geometry = new THREE.Geometry();
+
+geometry.vertices.push(
+	new THREE.Vector3( 0,  0, -4 ),
+	new THREE.Vector3( 4, 0, 0 ),
+	new THREE.Vector3(  0, 4, 0 )
+);
+
+geometry.faces.push(new THREE.Face3(0,1,2));
+
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+
+var mesh = new THREE.Mesh( geometry, material );
+VR.scene.add( mesh );
