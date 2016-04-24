ae8fd9039e5331a878d5ce0a9b18d8928b0fab32
SupaStuff <StuffQilla@gmail.com>
2016-04-23 23:48:43 -0400

    Used greyscale for height

--- run.py
+++ run.py
@@ -10,17 +10,26 @@ import os
 import png
 import numpy
 import itertools
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def getHeight():
-    r=png.Reader(file=urllib.urlopen('http://ms-mars.mars.asu.edu/?LAYERS=MOLA_Color&FORMAT=image/png&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&STYLES=&SRS=EPSG:4326&BBOX=-5.505,-11.25,0,-5.625&WIDTH=512&HEIGHT=512'))
-    image_2d = numpy.vstack(itertools.imap(numpy.uint8, r.asDirect()))
-    print(image_2d)
+    r=png.Reader(file=urllib.urlopen('http://ms-mars.mars.asu.edu/?LAYERS=MOLA_128ppd_shade_ne&FORMAT=image/png&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&STYLES=&SRS=EPSG:4326&BBOX=5.625,39.375,7.03125,40.78125&WIDTH=256&HEIGHT=256'))
+    rCount, cCount, pngData, metaData = r.asDirect()
 
-    return 0
+    image_2d = numpy.vstack(itertools.imap(numpy.uint8, pngData))
+    print(image_2d)
+    print(len(image_2d))
+    print(len(image_2d[:, :1]))
+    print(rCount)
+    print(cCount)
+    print(metaData)
+
+    return str(json.dumps(image_2d[:, 0::2].tolist()))
+    #return str(json.dumps(image_2d.tolist()))
 
 
 
